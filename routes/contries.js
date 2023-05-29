c879d7b8d71c97e4758129ac34554be072cdb157
unknown <xpmxf5@gmail.com>
2023-05-29 16:49:45 +0900

    ⭐ feat: isl 테이블에 법규 추가 api 작성

--- routes/contries.js
+++ routes/contries.js
@@ -1,21 +1,41 @@
 const express = require("express")
+const mysql = require("mysql")
+const fs = require("fs")
 const router = express.Router()
+
+const [host, user, password, database] = fs.readFileSync("../database_config.txt", "utf8").split("\n")
+
+const db = mysql.createConnection({
+    host,
+    user,
+    password,
+    database,
+})
 
 router.post("/", function (req, res) {
     const from_country = req.body.from_country
     const to_country = req.body.to_country
 
-    // 올바른 데이터를 받았는지 확인하는 로직
     if (from_country.length !== 2 || to_country.length !== 2) {
         return res.status(400).json({
             error: "Invalid country code",
         })
     }
-    // DB 에 저장하는 작업
 
-    res.json({
-        message: "Data Received",
-        from_country: from_country,
-        to_country: to_country,
+    const query = "INSERT INTO isl (from_country, to_country) VALUES (?, ?)"
+    db.query(query, [from_country, to_country], function (err, result) {
+        if (err) {
+            return res.status(500).json({
+                error: "Database error",
+            })
+        }
+
+        res.json({
+            message: "Data Received",
+            from_country: from_country,
+            to_country: to_country,
+        })
     })
 })
+
+module.exports = router
