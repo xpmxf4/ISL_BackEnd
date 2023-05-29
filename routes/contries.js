63619e6da5c075ace12e1bc0d1b88848bfc0ce25
unknown <xpmxf5@gmail.com>
2023-05-29 16:31:58 +0900

    ⭐ feat: from_country, to_country  유효값 체크

--- routes/contries.js
+++ routes/contries.js
@@ -6,6 +6,11 @@ router.post("/", function (req, res) {
     const to_country = req.body.to_country
 
     // 올바른 데이터를 받았는지 확인하는 로직
+    if (from_country.length !== 2 || to_country.length !== 2) {
+        return res.status(400).json({
+            error: "Invalid country code",
+        })
+    }
     // DB 에 저장하는 작업
 
     res.json({
