41206701ba5ba230a6119fc9eb1fde0b351fb249
dev-chpark <xpmxf5@gmail.com>
2023-05-30 14:39:20 +0900

    ⚙️ chore: / 경로에 대한 라우팅 코드 수정

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser())
 
 // routers
 app.use("/countries", countriesRouter)
-app.get("/", indexRouter)
+app.use("/", indexRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
